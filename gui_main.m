492a60a5bed025a2b8e02dd85ac385040bb2968e
FJ_PSI <don.edri@gmail.com>
2016-07-21 12:45:43 +0200

    fixing the mess

--- gui_main.m
+++ gui_main.m
@@ -1,4 +1,4 @@
-function [steam, coolant, facility, NC, distributions, file, BC, GHFS, MP,timing]=gui_main(plot_flag,st_state_flag,clear_flag,handles)
+function [steam, coolant, facility, NC, distributions, file, BC, GHFS, MP,timing]=gui_main(interactive_flag,st_state_flag,clear_flag,handles)
    %%  PICK A FOLDER FOR PROCCESSING  
    
     % move to file directory, based on default value stored in GUI
@@ -26,7 +26,10 @@ function [steam, coolant, facility, NC, distributions, file, BC, GHFS, MP,timing
     set(handles.file_path_disp,'String',directories{1});
     
     %get options for processing
-    options=textread('adv_options.txt','%s');
+    fid=fopen('adv_options.txt','rt');
+    options=textscan(fid,'%s');
+    options=options{1};
+    fclose(fid);
     options=str2double(options(2:2:numel(options)));
 
     
@@ -44,7 +47,7 @@ function [steam, coolant, facility, NC, distributions, file, BC, GHFS, MP,timing
                 disp(['Deleting file  ','processed_steady_data_',file_list{dir_counter}{fc},'.mat'])
             end
             
-            [steam(fc), coolant(fc), facility(fc), NC(fc), distributions(fc), file(fc),BC(fc), GHFS(fc), MP(fc),timing(fc)]=file_processing(plot_flag,file_list{dir_counter}{fc},directories{dir_counter},st_state_flag,options);
+            [steam(fc), coolant(fc), facility(fc), NC(fc), distributions(fc), file(fc),BC(fc), GHFS(fc), MP(fc),timing(fc)]=file_processing(interactive_flag,file_list{dir_counter}{fc},directories{dir_counter},st_state_flag,options);
 % below is for debugging
 %            [steam1, coolant1, facility1, NC1, distributions1, file1,BC1, GHFS1, MP1]=file_processing(plot_flag,file_list{dir_counter}{fc},directories{dir_counter});
 %            steam(fc)=steam1;
